c50a0730192ba6ed76ec4732cbd7dcae7bc8e6f2
moya <moyamauthoor@outlook.com>
2022-03-09 16:03:54 +1100

    Duplicate results update with higher mark

--- models/result.js
+++ models/result.js
@@ -1,13 +1,19 @@
 const db = require("../database");
 
+function find_result(studentFirstName, studentLastName, testId) {
+    let sql = `SELECT * 
+    FROM TestResults
+    WHERE student_first_name = '${studentFirstName}' AND student_last_name = '${studentLastName}' AND test_id = ${testId};`
+
+   return db.query(sql).then(res => res.rows);
+}
+
 function create(results) {
 
-    // need to ensure only quality data is provided to DB - add guard statements to ensure no duplication etc
+    // Ideally would include guard statements to screen data, e.g. avoid duplication as per brief.
 
-    for (const prop in results) {
+    results.forEach(result => {
         try {
-            // could destructure to clean this up
-            let result = results[prop];
             let scanDateTime = result["scanned-on"];
             let firstName = result["first-name"];
             let lastName = result["last-name"];
@@ -16,19 +22,40 @@ function create(results) {
             let { available, obtained } = result["summary-marks"]; 
             let marksPercentage = (+obtained / +available * 100).toFixed(2);
 
-            let sql = `INSERT INTO TestResults 
-            (scan_date, student_first_name, student_last_name, student_number, test_id, marks_available, marks_obtained, marks_percentage) 
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`
+            find_result(firstName, lastName, testId)
+                .then(dbRes => {
+                    if (dbRes.length > 0) {
+                        let higherAvailMark = dbRes.marks_available >= available 
+                            ? dbRes.marks_available 
+                            : available;
+                        let higherObtMark = dbRes.marks_obtained >= obtained 
+                            ? dbRes.marks_obtained 
+                            : obtained;
+                    
+                        let sql = `UPDATE Testresults 
+                        SET marks_available = $1, 
+                            marks_obtained = $2 
+                        WHERE student_first_name = $3 AND student_last_name = $4 AND test_id = $5;`
 
-            let values = [scanDateTime, firstName, lastName, studentNumber, testId, available, obtained, marksPercentage];
+                        let values = [higherObtMark, higherAvailMark, firstName, lastName, testId];
 
-            db.query(sql, values);
-
+                        db.query(sql, values);
+                    } else {
+                        let sql = `INSERT INTO TestResults (scan_date, student_first_name, student_last_name, student_number, test_id, marks_available, marks_obtained, marks_percentage) 
+                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`
+    
+                        let values = [scanDateTime, firstName, lastName, studentNumber, testId, available, obtained, marksPercentage];
+    
+                        db.query(sql, values);
+                    }
+                })
+  
         } catch(e) {
-            console.log(`Error with result ${result[prop]}`);
+            // Throw error here
         }
-    }
+    });
 }
+
 
 function find_aggregate_by_testId(testId) {
     let sql = `SELECT 
@@ -42,18 +69,6 @@ function find_aggregate_by_testId(testId) {
         COUNT(*)
     FROM TestResults 
     WHERE test_id = ${testId};`
-
-    // SELECT 
-    //     AVG(marks_percentage)::numeric(10,1) AS mean,
-    //     STDDEV_POP(marks_percentage)::numeric(10,1) AS stddev,
-    //     MIN(marks_percentage)::numeric(10,1),
-    //     MAX(marks_percentage)::numeric(10,1),
-    //     PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY TestResults.marks_percentage)::numeric(10,1) AS p25,
-    //     PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY TestResults.marks_percentage)::numeric(10,1) AS p50,
-    //     PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY TestResults.marks_percentage)::numeric(10,1) AS p75,
-    //     COUNT(*)
-    // FROM TestResults 
-    // WHERE test_id = 9863;
 
     return db.query(sql).then(res => res.rows);
 }
